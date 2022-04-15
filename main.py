f7f7f615e17cf2b45c42b7ae0847b7f997a878e3
Parikshit Hiwase <55256204+Parikshit001@users.noreply.github.com>
2022-04-15 05:21:20 +0000

    General View

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-# Write your code below this line 👇
+print("Hello World !!") #print is a python function and the text between the double quotes is the string
+print("Hello World !\nHello World") #\n is used to end the line and start the utilization of new line.
+print("Hello"+ "User") #"+" is used to concatenation. That is merging different string into one.
+#In Python a space is very important.
+#Using irregular spaces can cause Intentation error
